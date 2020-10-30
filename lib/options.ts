d498c42a5c6889eede3ecbf7949cf633bddd9d93
Fabian Roloff <fabian.roloff@vocatus.de>
2020-10-30 15:57:03 +0100

    add options interface

---
+++ lib/options.ts
@@ -0,0 +1,6 @@
+/**
+ * Qualtrics Login data
+ */
+export interface QualtricsOptions { apiToken: string; baseUrl: string, defaultDirectory: string, agent: any } // eslint-disable-line
+
+export interface FetchOptions { apiToken: string; baseUrl: string, agent: any } // eslint-disable-line
