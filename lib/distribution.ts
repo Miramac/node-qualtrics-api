6f4f08d24c0765e2fb99ac1313f8a1616e710b23
Fabian Roloff <fabian.roloff@vocatus.de>
2021-03-19 15:38:06 +0100

    fix expirationDate to ISOString

--- lib/distribution.ts
+++ lib/distribution.ts
@@ -32,7 +32,7 @@ export class Distribution {
       description: 'distribution description',
       action: 'CreateDistribution',
       // Default expirationDate: 90 days
-      expirationDate: new Date(new Date().setDate(new Date().getDate() + 90)).toLocaleDateString() + ' 00:00:00',
+      expirationDate: new Date(new Date().setDate(new Date().getDate() + 90)).toISOString(),
       mailingListId: mailingListId
     }
     // overwrite defaults, if available
